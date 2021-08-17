eff7de9be5784a84415cfb4fe463933d4641416a
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2021-08-17 21:55:48 +0200

    Create project with Android Studio

---
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/MainActivity.kt
@@ -0,0 +1,38 @@
+package de.lukasneugebauer.nextcloudcookbook
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import de.lukasneugebauer.nextcloudcookbook.ui.theme.NextcloudCookbookTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NextcloudCookbookTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NextcloudCookbookTheme {
+        Greeting("Android")
+    }
+}
