0111b55c3d0820e585fe7aa0114ab5486b7d023b
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2022-12-11 17:07:07 +0100

    Improve error screen ui

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/presentation/error/AbstractErrorScreen.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/presentation/error/AbstractErrorScreen.kt
@@ -3,22 +3,45 @@ package de.lukasneugebauer.nextcloudcookbook.core.presentation.error
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import de.lukasneugebauer.nextcloudcookbook.R
 import de.lukasneugebauer.nextcloudcookbook.core.presentation.ui.theme.NextcloudCookbookTheme
 import de.lukasneugebauer.nextcloudcookbook.core.util.UiText
 
 @Composable
-fun AbstractErrorScreen(uiText: UiText) {
+fun AbstractErrorScreen(
+    uiText: UiText,
+    modifier: Modifier = Modifier,
+    icon: ImageVector = Icons.Default.Warning,
+    iconContentDescription: UiText? = null
+) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = dimensionResource(id = R.dimen.padding_m)),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.Start
     ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = iconContentDescription?.asString(),
+            modifier = Modifier
+                .size(dimensionResource(id = R.dimen.error_icon_size))
+                .padding(bottom = dimensionResource(id = R.dimen.padding_s)),
+            tint = MaterialTheme.colors.onBackground
+        )
         Text(text = uiText.asString())
     }
 }
