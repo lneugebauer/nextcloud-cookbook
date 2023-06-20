78fa934c7af68377627f24f07a6126ddfced8801
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2023-06-20 21:09:04 +0200

    Update coil

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/presentation/components/AuthorizedImage.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/presentation/components/AuthorizedImage.kt
@@ -4,48 +4,16 @@ import androidx.appcompat.content.res.AppCompatResources
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
-import coil.compose.ImagePainter
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.request.ImageRequest
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
 import de.lukasneugebauer.nextcloudcookbook.R
 import de.lukasneugebauer.nextcloudcookbook.core.domain.model.LocalCredentials
-import de.lukasneugebauer.nextcloudcookbook.core.util.Constants
-
-@Composable
-fun authorizedImagePainter(imageUrl: String): Painter {
-    val credentials = LocalCredentials.current
-
-    val path = credentials?.baseUrl?.toUri()?.path
-    val regex = """^$path""".toRegex()
-    val newImageUrl = imageUrl.replace(regex, "")
-    val fullImageUrl = credentials?.baseUrl + newImageUrl
-
-    val painter = rememberImagePainter(
-        data = fullImageUrl,
-        builder = {
-            credentials?.basic?.let {
-                addHeader("Authorization", credentials.basic)
-            }
-            crossfade(Constants.CROSSFADE_DURATION_MILLIS)
-        },
-    )
-
-    val painterState = painter.state
-    if (painterState is ImagePainter.State.Error) {
-        return rememberDrawablePainter(
-            drawable = AppCompatResources.getDrawable(
-                LocalContext.current,
-                R.drawable.common_image_placeholder,
-            ),
-        )
-    }
-
-    return painter
-}
 
 @Composable
 fun AuthorizedImage(
@@ -53,10 +21,42 @@ fun AuthorizedImage(
     contentDescription: String,
     modifier: Modifier,
 ) {
-    Image(
-        painter = authorizedImagePainter(imageUrl),
+    val context = LocalContext.current
+    val credentials = LocalCredentials.current
+
+    val path = credentials?.baseUrl?.toUri()?.path
+    val regex = """^$path""".toRegex()
+    val newImageUrl = imageUrl.replace(regex, "")
+    val fullImageUrl = credentials?.baseUrl + newImageUrl
+
+    SubcomposeAsyncImage(
+        model = ImageRequest.Builder(context)
+            .data(fullImageUrl)
+            .addHeader("Authorization", credentials?.basic ?: "")
+            .crossfade(true)
+            .build(),
         contentDescription = contentDescription,
-        modifier = modifier,
         contentScale = ContentScale.Crop,
-    )
+        modifier = modifier,
+    ) {
+        when (painter.state) {
+            is AsyncImagePainter.State.Error -> {
+                Image(
+                    painter = rememberDrawablePainter(
+                        drawable = AppCompatResources.getDrawable(
+                            context,
+                            R.drawable.common_image_placeholder,
+                        ),
+                    ),
+                    contentDescription = contentDescription,
+                    modifier = modifier,
+                    contentScale = ContentScale.Crop,
+                )
+            }
+
+            else -> {
+                SubcomposeAsyncImageContent()
+            }
+        }
+    }
 }
--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/util/Constants.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/util/Constants.kt
@@ -6,6 +6,5 @@ object Constants {
     private const val API_ENDPOINT: String = "index.php/apps/cookbook/api"
     private const val API_VERSION: String = "/v1"
     const val FULL_PATH: String = API_ENDPOINT + API_VERSION
-    const val CROSSFADE_DURATION_MILLIS: Int = 750
     const val SHARED_PREFERENCES_KEY: String = BuildConfig.APPLICATION_ID + ".SHARED_PREFERENCES"
 }
