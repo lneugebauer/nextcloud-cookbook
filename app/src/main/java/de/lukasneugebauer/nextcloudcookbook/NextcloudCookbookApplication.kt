ce7fb749510c43d634958b037d09b7e738544e9d
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2023-10-11 14:30:03 +0200

    Add crash reporting

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/NextcloudCookbookApplication.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/NextcloudCookbookApplication.kt
@@ -1,8 +1,14 @@
 package de.lukasneugebauer.nextcloudcookbook
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
+import org.acra.config.dialog
+import org.acra.config.mailSender
+import org.acra.data.StringFormat
+import org.acra.ktx.initAcra
 import timber.log.Timber
+import java.time.LocalDate
 
 @HiltAndroidApp
 class NextcloudCookbookApplication : Application() {
@@ -16,6 +22,32 @@ class NextcloudCookbookApplication : Application() {
     private fun initializeTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+
+        if (!BuildConfig.DEBUG) {
+            initAcra {
+                buildConfigClass = BuildConfig::class.java
+                reportFormat = StringFormat.JSON
+
+                dialog {
+                    title = getString(R.string.app_name)
+                    text = getString(R.string.dialog_crash_report_text)
+                    negativeButtonText = getString(R.string.common_no)
+                    positiveButtonText = getString(R.string.common_yes)
+                    resTheme = android.R.style.Theme_DeviceDefault_Dialog
+                }
+
+                mailSender {
+                    mailTo = "kontakt+nextcloudcookbook@lukasneugebauer.de"
+                    reportFileName =
+                        "ACRA-report_${LocalDate.now()}_v${BuildConfig.VERSION_NAME}-${BuildConfig.VERSION_CODE}.txt"
+                    subject = "Nextcloud Cookbook crash report"
+                }
+            }
         }
     }
 }
