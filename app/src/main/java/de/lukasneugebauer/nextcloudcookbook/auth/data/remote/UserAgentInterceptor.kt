433fc331ef915dcab6041cfffc079a43f636b381
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2023-10-22 11:51:19 +0200

    Fix user agent crash in some languages

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/auth/data/remote/UserAgentInterceptor.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/auth/data/remote/UserAgentInterceptor.kt
@@ -1,10 +1,7 @@
 package de.lukasneugebauer.nextcloudcookbook.auth.data.remote
 
-import android.content.Context
 import android.os.Build
-import dagger.hilt.android.qualifiers.ApplicationContext
 import de.lukasneugebauer.nextcloudcookbook.BuildConfig
-import de.lukasneugebauer.nextcloudcookbook.R
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.util.Locale
@@ -13,15 +10,13 @@ import javax.inject.Singleton
 
 // https://gist.github.com/twaddington/e66e495e14950b4437216ab5c704835b
 @Singleton
-class UserAgentInterceptor @Inject constructor(
-    @ApplicationContext private val context: Context,
-) : Interceptor {
+class UserAgentInterceptor @Inject constructor() : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val userAgent = String.format(
             Locale.US,
             "%s/%s (Android %s; %s; %s %s; %s)",
-            context.getString(R.string.app_name),
+            "Cookbook",
             BuildConfig.VERSION_NAME,
             Build.VERSION.RELEASE,
             Build.MODEL,
@@ -34,6 +29,7 @@ class UserAgentInterceptor @Inject constructor(
             .newBuilder()
             .header("User-Agent", userAgent)
             .build()
+
         return chain.proceed(request)
     }
 }
