847572621364c667c1ef286fe63e10a4c561ece8
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2024-02-01 21:47:42 +0100

    Fix production build

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/data/dto/NextcloudVersionDto.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/data/dto/NextcloudVersionDto.kt
@@ -1,8 +1,10 @@
 package de.lukasneugebauer.nextcloudcookbook.core.data.dto
 
+import com.google.gson.annotations.SerializedName
 import de.lukasneugebauer.nextcloudcookbook.core.domain.model.NextcloudVersion
 
 data class NextcloudVersionDto(
+    @SerializedName("string")
     val string: String,
 ) {
     fun toNextcloudVersion(): NextcloudVersion = string
