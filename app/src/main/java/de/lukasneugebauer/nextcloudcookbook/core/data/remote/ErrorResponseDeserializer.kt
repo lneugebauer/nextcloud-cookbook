08c5fea17549a8a9519a3463d9b83146aa258cac
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2024-07-29 22:22:05 +0200

    Refresh caches after import

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/data/remote/ErrorResponseDeserializer.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/data/remote/ErrorResponseDeserializer.kt
@@ -4,22 +4,21 @@ import com.google.gson.Gson
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
-import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonToken
 import de.lukasneugebauer.nextcloudcookbook.core.data.remote.response.ErrorResponse
 import timber.log.Timber
 import java.io.StringReader
 import java.lang.reflect.Type
 
-class ErrorResponseDeserializer: JsonDeserializer<ErrorResponse> {
+class ErrorResponseDeserializer : JsonDeserializer<ErrorResponse> {
     override fun deserialize(
         json: JsonElement?,
         typeOfT: Type?,
-        context: JsonDeserializationContext?
+        context: JsonDeserializationContext?,
     ): ErrorResponse {
         val gson = Gson()
         val reader = gson.newJsonReader(StringReader(json.toString()))
-        return when(reader.peek()) {
+        return when (reader.peek()) {
             JsonToken.BEGIN_OBJECT -> gson.fromJson(json, typeOfT)
             JsonToken.STRING -> ErrorResponse(msg = gson.fromJson(json, String::class.java))
             else -> {
