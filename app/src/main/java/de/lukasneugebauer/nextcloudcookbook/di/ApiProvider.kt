a66899963d17b06857f6a65a896aa1aac19b0dde
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2024-08-19 21:36:17 +0200

    Increase timeout duration

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/di/ApiProvider.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/di/ApiProvider.kt
@@ -22,6 +22,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -74,6 +75,9 @@ class ApiProvider
                     .addInterceptor(httpLoggingInterceptor)
                     .addInterceptor(authInterceptor)
                     .addNetworkInterceptor(NetworkInterceptor())
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
                     .build()
 
             val retrofit =
