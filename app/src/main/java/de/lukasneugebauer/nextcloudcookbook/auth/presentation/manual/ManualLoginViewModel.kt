a67174cddf7e4777f1e4c77c65b9332a69aec9e6
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2025-06-13 23:15:47 +0200

    wip: refactor: login flow

---
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/auth/presentation/manual/ManualLoginViewModel.kt
@@ -0,0 +1,8 @@
+package de.lukasneugebauer.nextcloudcookbook.auth.presentation.manual
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ManualLoginViewModel
+    @Inject
+    constructor() : ViewModel()
--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/recipe/presentation/list/RecipeListScreen.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/recipe/presentation/list/RecipeListScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Sort
@@ -35,6 +34,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -493,7 +493,7 @@ private fun SearchAppBar(
             placeholder = {
                 Text(
                     text = stringResource(R.string.common_search),
-                    modifier = Modifier.alpha(ContentAlpha.medium),
+                    color = LocalContentColor.current.copy(alpha = 0.74f),
                 )
             },
             leadingIcon = {
@@ -501,7 +501,7 @@ private fun SearchAppBar(
                     Icon(
                         imageVector = Icons.Default.Search,
                         contentDescription = null,
-                        modifier = Modifier.alpha(ContentAlpha.medium),
+                        tint = LocalContentColor.current.copy(alpha = 0.74f),
                     )
                 }
             },
