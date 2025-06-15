957dec9838f0a53a362d6b15b5808520718121ef
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2025-06-15 22:29:03 +0200

    wip: refactor: manual login

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/auth/presentation/manual/ManualLoginScreen.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/auth/presentation/manual/ManualLoginScreen.kt
@@ -1,26 +1,33 @@
 package de.lukasneugebauer.nextcloudcookbook.auth.presentation.manual
 
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -31,139 +38,196 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.generated.destinations.HomeScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.StartScreenDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import de.lukasneugebauer.nextcloudcookbook.R
+import de.lukasneugebauer.nextcloudcookbook.auth.domain.state.ManualLoginScreenState
 import de.lukasneugebauer.nextcloudcookbook.core.presentation.MainGraph
 import de.lukasneugebauer.nextcloudcookbook.core.presentation.components.DefaultButton
 import de.lukasneugebauer.nextcloudcookbook.core.presentation.components.DefaultOutlinedTextField
+import de.lukasneugebauer.nextcloudcookbook.core.presentation.components.HideBottomNavigation
+import de.lukasneugebauer.nextcloudcookbook.core.presentation.components.Loader
+import de.lukasneugebauer.nextcloudcookbook.core.presentation.error.AbstractErrorScreen
 import de.lukasneugebauer.nextcloudcookbook.core.presentation.ui.theme.NextcloudCookbookTheme
 import de.lukasneugebauer.nextcloudcookbook.core.util.UiText
 
 @Destination<MainGraph>
 @Composable
-fun ManualLoginScreen(
+fun AnimatedVisibilityScope.ManualLoginScreen(
     navigator: DestinationsNavigator,
     url: String,
     allowSelfSignedCertificates: Boolean,
     viewModel: ManualLoginViewModel = hiltViewModel(),
 ) {
-    ManualLoginLayout()
+    val uiState by viewModel.uiState.collectAsState()
+
+    HideBottomNavigation()
+
+    LaunchedEffect(uiState) {
+        if (uiState is ManualLoginScreenState.Authenticated) {
+            navigator.navigate(HomeScreenDestination) {
+                popUpTo(StartScreenDestination) {
+                    inclusive = true
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        topBar = { TopAppBar(onBackClick = {navigator.navigateUp()})}
+    ) { innerPadding ->
+        when (uiState) {
+            is ManualLoginScreenState.Loaded -> {
+                val (username, password, usernameError, passwordError) = uiState as ManualLoginScreenState.Loaded
+                ManualLoginLayout(
+                    url = url,
+                    username = username,
+                    password = password,
+                    modifier = Modifier.padding(innerPadding),
+                    usernameError = usernameError,
+                    passwordError = passwordError,
+                    onUsernameChange = { username ->
+                        viewModel.onUsernameChange(username)
+                    },
+                    onPasswordChange = { password ->
+                        viewModel.onPasswordChange(password)
+                    },
+                    onLoginClick = {
+                        viewModel.tryManualLogin()
+                    }
+                )
+            }
+            ManualLoginScreenState.Authenticated, is ManualLoginScreenState.Authenticating -> {
+                Loader(modifier = Modifier.padding(innerPadding))
+            }
+            is ManualLoginScreenState.Error -> {
+                val message = (uiState as ManualLoginScreenState.Error).uiText
+                AbstractErrorScreen(uiText = message, modifier = Modifier.padding(innerPadding))
+            }
+        }
+    }
 }
 
 @Composable
-fun ManualLoginLayout(modifier: Modifier = Modifier) {
-    // TODO: Show form with username and password. Do authentication check after button click.
-    //  Redirect to home screen if successful.
-}
-
-@Composable
-private fun ManualLoginForm(
-    usernameError: UiText?,
-    passwordError: UiText?,
+fun ManualLoginLayout(
     url: String,
-    onClearError: () -> Unit,
-    onManualLoginClick: (username: String, password: String, url: String) -> Unit,
+    username: String,
+    password: String,
+    modifier: Modifier = Modifier,
+    usernameError: UiText? = null,
+    passwordError: UiText? = null,
+    onUsernameChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onLoginClick: () -> Unit,
 ) {
     val focusManager = LocalFocusManager.current
-    var username: String by rememberSaveable { mutableStateOf("") }
-    var password: String by rememberSaveable { mutableStateOf("") }
     var passwordVisibility by rememberSaveable { mutableStateOf(false) }
-    DefaultOutlinedTextField(
-        value = username,
-        onValueChange = {
-            username = it
-            onClearError.invoke()
-        },
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = dimensionResource(id = R.dimen.padding_m)),
-        label = {
-            Text(
-                text = stringResource(R.string.common_username),
-            )
-        },
-        errorText = usernameError?.asString(),
-        keyboardOptions =
-            KeyboardOptions.Default.copy(
-                imeAction = ImeAction.Next,
-            ),
-        keyboardActions =
-            KeyboardActions(
-                onNext = {
-                    focusManager.moveFocus(FocusDirection.Down)
-                },
-            ),
-        singleLine = true,
-    )
-    Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_s)))
-    DefaultOutlinedTextField(
-        value = password,
-        onValueChange = {
-            password = it
-            onClearError.invoke()
-        },
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = dimensionResource(id = R.dimen.padding_m)),
-        label = {
-            Text(
-                text = stringResource(R.string.common_password),
-            )
-        },
-        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-        trailingIcon = {
-            val image =
-                if (passwordVisibility) {
-                    Icons.Filled.Visibility
-                } else {
-                    Icons.Filled.VisibilityOff
-                }
 
-            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
-                Icon(imageVector = image, "Show/hide password", tint = Color.White)
-            }
-        },
-        errorText = passwordError?.asString(),
-        keyboardOptions =
-            KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Next,
-            ),
-        keyboardActions =
-            KeyboardActions(
-                onNext = {
-                    focusManager.moveFocus(FocusDirection.Down)
-                },
-            ),
-        singleLine = true,
-    )
-    Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_s)))
-    Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_s)))
-    DefaultButton(
-        onClick = { onManualLoginClick(username, password, url) },
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = dimensionResource(id = R.dimen.padding_m)),
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_m)),
+        verticalArrangement = Arrangement.Center,
     ) {
-        Text(text = stringResource(R.string.login_manual))
+        Text(
+            text = stringResource(R.string.login_instance_hint, url),
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_m)),
+        )
+        DefaultOutlinedTextField(
+            value = username,
+            onValueChange = onUsernameChange,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = stringResource(R.string.common_username))
+            },
+            errorText = usernameError?.asString(),
+            keyboardOptions =
+                KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Next,
+                ),
+            keyboardActions =
+                KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    },
+                ),
+            singleLine = true,
+        )
+        Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_s)))
+        DefaultOutlinedTextField(
+            value = password,
+            onValueChange = onPasswordChange,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = stringResource(R.string.common_password))
+            },
+            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image =
+                    if (passwordVisibility) {
+                        Icons.Filled.Visibility
+                    } else {
+                        Icons.Filled.VisibilityOff
+                    }
+
+                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                    Icon(imageVector = image, "Show/hide password")
+                }
+            },
+            errorText = passwordError?.asString(),
+            keyboardOptions =
+                KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next,
+                ),
+            keyboardActions =
+                KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    },
+                ),
+            singleLine = true,
+        )
+        Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_s)))
+        Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_s)))
+        DefaultButton(
+            onClick = onLoginClick,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(text = stringResource(R.string.login))
+        }
     }
+}
+
+@Composable
+private fun TopAppBar(onBackClick: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(R.string.login))
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = stringResource(R.string.common_back),
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun ManualLoginFormPreview() {
+private fun ManualLoginLayoutPreview() {
     NextcloudCookbookTheme {
-        Column {
-            ManualLoginForm(
-                usernameError = null,
-                passwordError = null,
-                url = "",
-                onClearError = {},
-                onManualLoginClick = { _, _, _ -> },
-            )
-        }
+        ManualLoginLayout(
+            url = "https://cloud.example.com",
+            username = "foo",
+            password = "bar",
+            onUsernameChange = {},
+            onPasswordChange = {},
+            onLoginClick = {},
+        )
     }
 }
--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/auth/presentation/manual/ManualLoginViewModel.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/auth/presentation/manual/ManualLoginViewModel.kt
@@ -1,10 +1,150 @@
 package de.lukasneugebauer.nextcloudcookbook.auth.presentation.manual
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import de.lukasneugebauer.nextcloudcookbook.R
+import de.lukasneugebauer.nextcloudcookbook.auth.domain.state.ManualLoginScreenState
+import de.lukasneugebauer.nextcloudcookbook.core.data.PreferencesManager
+import de.lukasneugebauer.nextcloudcookbook.core.domain.model.NcAccount
+import de.lukasneugebauer.nextcloudcookbook.core.domain.repository.AccountRepository
+import de.lukasneugebauer.nextcloudcookbook.core.domain.usecase.ClearPreferencesUseCase
+import de.lukasneugebauer.nextcloudcookbook.core.util.Resource
+import de.lukasneugebauer.nextcloudcookbook.core.util.UiText
+import de.lukasneugebauer.nextcloudcookbook.di.ApiProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ManualLoginViewModel
     @Inject
-    constructor() : ViewModel()
+    constructor(
+        private val accountRepository: AccountRepository,
+        private val apiProvider: ApiProvider,
+        private val clearPreferencesUseCase: ClearPreferencesUseCase,
+        private val preferencesManager: PreferencesManager,
+        private val savedStateHandle: SavedStateHandle,
+    ) : ViewModel() {
+        private val _uiState = MutableStateFlow<ManualLoginScreenState>(ManualLoginScreenState.Loaded())
+        val uiState = _uiState.asStateFlow()
+
+        init {
+            observeAuthorizationStatus()
+        }
+
+        fun onUsernameChange(newUsername: String) {
+            _uiState.update {
+                if (it is ManualLoginScreenState.Loaded) {
+                    it.copy(username = newUsername, usernameError = null, passwordError = null)
+                } else {
+                    it
+                }
+            }
+        }
+
+        fun onPasswordChange(newPassword: String) {
+            _uiState.update {
+                if (it is ManualLoginScreenState.Loaded) {
+                    it.copy(password = newPassword, usernameError = null, passwordError = null)
+                } else {
+                    it
+                }
+            }
+        }
+
+        fun tryManualLogin() {
+            val url: String? = savedStateHandle["url"]
+            if (url == null) {
+                _uiState.update { ManualLoginScreenState.Error(uiText = UiText.StringResource(R.string.error_invalid_url)) }
+                return
+            }
+
+            val (username, password) = _uiState.value as? ManualLoginScreenState.Loaded ?: return
+
+            if (!isValidUsername(username)) return
+            if (!isValidPassword(password)) return
+
+            _uiState.update {
+                ManualLoginScreenState.Authenticating(username = username, password = password)
+            }
+
+            val ncAccount = NcAccount(
+                name = "",
+                username = username,
+                token = password,
+                url = url.removeSuffix("/"),
+            )
+
+            viewModelScope.launch {
+                preferencesManager.updateNextcloudAccount(ncAccount)
+                apiProvider.initApi()
+            }
+        }
+
+        private fun observeAuthorizationStatus() {
+            viewModelScope.launch {
+                combine(
+                    accountRepository.getAccount(),
+                    apiProvider.ncCookbookApiFlow,
+                ) { account, api -> Pair(account, api) }
+                    .distinctUntilChanged()
+                    .collect { (account, api) ->
+                        when {
+                            api == null -> Unit
+
+                            account is Resource.Error -> Unit
+
+                            account is Resource.Success -> {
+                                val userMetadata = accountRepository.getUserMetadata()
+                                if (userMetadata is Resource.Error) {
+                                    clearPreferencesUseCase()
+                                    _uiState.update {
+                                        ManualLoginScreenState.Error(
+                                            uiText = userMetadata.message ?: UiText.StringResource(R.string.error_unknown),
+                                        )
+                                    }
+                                } else {
+                                    _uiState.update { ManualLoginScreenState.Authenticated }
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+
+        private fun isValidUsername(username: String): Boolean {
+            if (username.isBlank()) {
+                _uiState.update {
+                    if (it is ManualLoginScreenState.Loaded) {
+                        it.copy(usernameError = UiText.StringResource(R.string.error_empty_username))
+                    } else {
+                        it
+                    }
+                }
+                return false
+            }
+
+            return true
+        }
+
+        private fun isValidPassword(password: String): Boolean {
+            if (password.isBlank()) {
+                _uiState.update {
+                    if (it is ManualLoginScreenState.Loaded) {
+                        it.copy(passwordError = UiText.StringResource(R.string.error_empty_password))
+                    } else {
+                        it
+                    }
+                }
+                return false
+            }
+
+            return true
+        }
+    }
