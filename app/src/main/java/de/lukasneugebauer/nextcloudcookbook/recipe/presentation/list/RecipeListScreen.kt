fa11ed92fc325fea57e65caa9ce0280883e4282b
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2025-06-23 22:12:09 +0200

    fix: recipe list screen title wrap

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/recipe/presentation/list/RecipeListScreen.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/recipe/presentation/list/RecipeListScreen.kt
@@ -48,7 +48,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -59,6 +58,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
@@ -282,7 +282,13 @@ private fun TopAppBar(
     val title = categoryName ?: stringResource(id = R.string.common_recipes)
 
     TopAppBar(
-        title = { Text(text = title) },
+        title = {
+            Text(
+                text = title,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+            )
+        },
         navigationIcon = {
             if (categoryName != null) {
                 IconButton(onClick = onBackClick) {
