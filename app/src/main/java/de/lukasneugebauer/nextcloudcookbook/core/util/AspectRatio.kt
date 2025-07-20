e095bd0cb93807a456ee1e916ebc83fda6fd113f
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2025-07-20 15:55:27 +0200

    style: fix ktlint issues

--- app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/util/AspectRatio.kt
+++ app/src/main/java/de/lukasneugebauer/nextcloudcookbook/core/util/AspectRatio.kt
@@ -3,5 +3,5 @@ package de.lukasneugebauer.nextcloudcookbook.core.util
 enum class AspectRatio(val ratio: Float) {
     PHOTO(4f / 3f),
     VIDEO(16f / 9f),
-    CINEMA_SCOPE(2.4f / 1f)
+    CINEMA_SCOPE(2.4f / 1f),
 }
