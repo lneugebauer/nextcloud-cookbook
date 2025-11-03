2bfc70a60858a304ee7460013f6bc1a99de0145f
lselter <LSelter@kawasakihydraulics.com>
2025-11-03 17:33:39 +0000

    misc changes

--- app/src/androidTest/java/de/lukasneugebauer/nextcloudcookbook/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/de/lukasneugebauer/nextcloudcookbook/ExampleInstrumentedTest.kt
@@ -1,10 +1,14 @@
 package de.lukasneugebauer.nextcloudcookbook
 
+import android.content.pm.PackageManager
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.System.console
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,8 +19,19 @@ import org.junit.runner.RunWith
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("de.lukasneugebauer.nextcloudcookbook", appContext.packageName)
+        assertNotNull("App context should not be null", appContext)
+
+        val pkg = appContext.packageName
+        val expectedPrefix = "de.lukasneugebauer.nextcloudcookbook"
+        assertTrue("Package name should start with $expectedPrefix; actual: $pkg", pkg.startsWith(expectedPrefix))
+
+        val pm = appContext.packageManager
+        try {
+            val appInfo = pm.getApplicationInfo(pkg, 0)
+            assertNotNull("ApplicationInfo should be retrievable for $pkg", appInfo)
+        } catch (e: PackageManager.NameNotFoundException) {
+            throw AssertionError("Package not found: $pkg", e)
+        }
     }
 }
